0bc65576a7159770cec28295d89e5db11db87a4d
lama <lawr.styles@gmail.com>
2023-02-18 07:15:38 +0200

    Add stage environment

---
+++ stage/level2/variables.tf
@@ -0,0 +1,15 @@
+variable "env_code" {
+  type = string
+}
+
+variable "db_name" {
+  default = "mydb"
+}
+
+variable "db_username" {
+  default = "admin"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
